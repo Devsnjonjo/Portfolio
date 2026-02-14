be8577515dabedd4eae5aaeaf1e46410117d8664
devsnjonjo <dnjonjo1@gmail.com>
2026-02-14 13:42:00 +0300

    commit

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
-# <!-- creat an instance of the class -->
-app = Flask(__name__)
-# <!-- routes -->
+from flask import Flask,render_template
+
+# create an instance of the class
+app =Flask(__name__)
+
+# routes/
 @app.route('/')
 def index():
     return render_template('index.html')
-    # <!-- other routes -->
+# other routes 
+
+# about route 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
