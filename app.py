aafe40aab9efecb77edba887b6093c1d9027c8b4
devsnjonjo <dnjonjo1@gmail.com>
2026-02-16 21:02:31 +0300

    Monday 16th

--- app.py
+++ app.py
@@ -13,6 +13,29 @@ def index():
 @app.route('/about')
 def about():
     return render_template('about.html')
+@app.route('/portfolio')
+def portfolio():
+    projects = [
+        {
+            'id': 'Project-1',
+            'title': 'Project One',
+            'description': 'My first Project built with Bootstrap',
+            'image': 'p1.jpg'
+        },
+        {
+            'id': 'Project-2',
+            'title': 'Project Two',
+            'description': 'My second Project built with Flask',
+            'image': 'p2.jpg'
+        },
+        {
+            'id': 'Project-3',
+            'title': 'Project Three',
+            'description': 'My third Project built with Vue.js',
+            'image': 'p3.jpg'
+        }
+    ]
+    return render_template('portfolio.html',projects=projects)
 
 if __name__ == '__main__':
     app.run(debug=True)
